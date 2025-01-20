9262da5ee3e77fc8c1556cfaf3f77a55474951d5
2AW3 User 17 <2AW3-17@yourdomain.com>
2025-01-20 11:06:39 +0100

    Código con comentarios, preparado para transmisión

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,24 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.scss']
 })
-export class ProyectosComponent implements AfterViewInit {
-
-  ngAfterViewInit() {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.remove('hidden');
-        } else {
-          entry.target.classList.add('hidden');
-        }
-      });
-    });
-
-    const hiddenElements = document.querySelectorAll('.animatable');
-    hiddenElements.forEach(el => observer.observe(el));
-  }
+export class ProyectosComponent {
 }
