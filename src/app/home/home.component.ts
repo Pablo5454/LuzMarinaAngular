a296e8852d31e95d95284d24c36c75e78f0f3e82
Pablo Perucha <p_peruchacab@fpzornotza.com>
2025-01-26 13:52:02 +0100

    Carrusel terminado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit, OnDestroy {
+  slides: string[] = [
+    'assets/imagenes/img1.jpg',
+    'assets/imagenes/img2.webp',
+    'assets/imagenes/img3.jpg'
+  ];
+
+  currentSlide: number = 0;
+  slideInterval: any;
+
+  ngOnInit() {
+    this.startSlideShow();
+  }
+
+  ngOnDestroy() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 5000);
+  }
 }
